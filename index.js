cddd8c4c0a8a19e2d1662094307a40cad49926ed
Junaid Khan <86207985+Junaid1240651@users.noreply.github.com>
2023-08-31 00:32:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,54 @@ mongoose
 
 app.get("/", async (req, res) => {
  try {
-    const result = await ChartData.find({});
+     const {
+      endYear,
+      topics,
+      sector,
+      region,
+      pest,
+      source,
+      country,
+      limit,
+    } = req.query;
+
+    let query = ChartData.find({});
+
+    if (endYear) {
+      query = query.where("end_year").equals(parseInt(endYear));
+    }
+
+    if (topics) {
+      const topicsArray = topics.split(",");
+      query = query.where("topic").in(topicsArray);
+    }
+
+    if (sector) {
+      query = query.where("sector").equals(sector);
+    }
+
+    if (region) {
+      query = query.where("region").equals(region);
+    }
+
+    if (pest) {
+      query = query.where("pestle").equals(pest);
+    }
+
+    if (source) {
+      query = query.where("source").equals(source);
+    }
+
+    if (country) {
+      query = query.where("country").equals(country);
+    }
+
+    if (limit) {
+      query = query.limit(parseInt(limit));
+    }
+
+    const result = await query.exec();
+ 
     res.status(200).json(result);
      } catch (error) {
     res.status(500).json({ error: "Error retrieving data" });
